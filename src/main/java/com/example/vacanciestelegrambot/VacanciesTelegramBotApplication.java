1d1891f7c0552569abafc9da40ba84362c72b704
mIst <bogdankharchenko77@gmail.com>
2023-12-19 21:23:21 +0200

    project created

---
+++ src/main/java/com/example/vacanciestelegrambot/VacanciesTelegramBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.vacanciestelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanciesTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacanciesTelegramBotApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/vacanciestelegrambot/VacanciesTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacanciestelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
